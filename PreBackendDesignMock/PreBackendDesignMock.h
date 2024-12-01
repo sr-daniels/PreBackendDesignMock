9cdac2419ccb48ac832984ef48c48959b54c0221
Sharonda Daniels <sharonda.d27@gmail.com>
2024-11-30 23:08:42 -0800

    Initial Commit

---
+++ PreBackendDesignMock/PreBackendDesignMock.h
@@ -0,0 +1,18 @@
+//
+//  PreBackendDesignMock.h
+//  PreBackendDesignMock
+//
+//  Created by Sharonda Daniels on 11/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PreBackendDesignMock.
+FOUNDATION_EXPORT double PreBackendDesignMockVersionNumber;
+
+//! Project version string for PreBackendDesignMock.
+FOUNDATION_EXPORT const unsigned char PreBackendDesignMockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreBackendDesignMock/PublicHeader.h>
+
+
---
+++ PreBackendDesignMockTests/PreBackendDesignMockTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreBackendDesignMockTests.swift
+//  PreBackendDesignMockTests
+//
+//  Created by Sharonda Daniels on 11/30/24.
+//
+
+import Testing
+@testable import PreBackendDesignMock
+
+struct PreBackendDesignMockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
