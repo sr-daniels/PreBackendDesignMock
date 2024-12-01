eaf13798dd37a971267cde740c908a0e14fe266e
Sharonda Daniels <sharonda.d27@gmail.com>
2024-11-30 23:16:11 -0800

    Create the FeedItem module

---
+++ PreBackendDesignMock/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  PreBackendDesignMock
+//
+//  Created by Sharonda Daniels on 11/30/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
+
